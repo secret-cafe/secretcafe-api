b2aef26ec8ad1bb0d99fdcbef972b516ceaf5d41
iamdeveloperdevesh <secretcafe372@gmail.com>
2026-04-17 17:39:41 +0530

    Minor changes

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -10,7 +10,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
             user: process.env.DATABASE_USER,
             password: process.env.DATABASE_PASSWORD,
             database: process.env.DATABASE_NAME,
-            connectionLimit: 5,
+            connectionLimit: 1,
         });
 
         super({ adapter });

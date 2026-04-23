c38c2528c9c19eb14c61e75e1e4888cf41bb5901
iamdeveloperdevesh <secretcafe372@gmail.com>
2026-04-23 09:51:49 +0530

    Cookie Issue Fixing

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -16,10 +16,12 @@ export class AuthController {
     );
 
     res.cookie('token', result.access_token, {
+      // domain: '.vercel.app',
       httpOnly: true,
       secure: true,
       sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
+      path: '/',
     });
 
     return {
